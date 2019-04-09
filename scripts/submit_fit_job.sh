91e3537e3d2c865f6ed2df8db78d792aec237586
cuddandr <cuddandr@msu.edu>
2019-04-09 09:42:31 -0400

    Updates to job submission script for job arrays.
    Updated the job script and the submission script to submit a job array
    instead of a single job.
    
    A single job can still be used by only specifying one job when running
    the submission script.
    
    All jobs use the same configuration file, but the output file and RNG
    seed change based on the job ID index.

--- scripts/submit_fit_job.sh
+++ scripts/submit_fit_job.sh
@@ -14,14 +14,22 @@ if [ -z ${1} ]; then
     exit 64
 fi
 
+if [ -z ${2} ]; then
+    NUMJOBS=0
+else
+    NUMJOBS=$((${2}-1))
+fi
+
 THREADS=$(grep -i "num_threads" ${1} | sed 's/[^0-9]*//g')
 CONFIG=$(basename ${1})
 
 sed -i "/#SBATCH --cpus-per-task/c\#SBATCH --cpus-per-task=${THREADS}" ${FITROOT}/scripts/fit_job.sb
-sed -i "/xsllhFit -j/c\xsllhFit -j ${CONFIG} &> fit_output.log" ${FITROOT}/scripts/fit_job.sb
+sed -i "/#SBATCH --array/c\#SBATCH --array=0-${NUMJOBS}" ${FITROOT}/scripts/fit_job.sb
+sed -i "/FITCONFIG=/c\FITCONFIG=\"${CONFIG}\"" ${FITROOT}/scripts/fit_job.sb
 
 echo "Config File: ${1}"
 echo "Threads: ${THREADS}"
+echo "Num Jobs: $(($NUMJOBS+1))"
 
 cp ${1} $RUNPATH
 cd $RUNPATH
