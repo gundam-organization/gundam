c4d5f3c2e01124b0ef2453e1c43b2193523b92d1
cuddandr <cuddandr@msu.edu>
2019-04-10 10:11:17 -0400

    Further updates to job scripts. New script for chisq calculation.
    A couple more updates to the job submission scripts. The output files
    now use the seed as part of the file name.
    
    The script to setup the job file now has the ability to specify the seed
    offset or use a random value.
    
    New script to calculate the chisquare between two histograms,
    specifically the selected and truth cross-sections from the error
    propagation output.

---
+++ scripts/calc_chisq.cxx
@@ -0,0 +1,99 @@
+#include <RVersion.h>
+double calc_chisq(std::string filename, bool is_txt_file = false)
+{
+    if(ROOT_VERSION_CODE < ROOT_VERSION(6,0,0))
+    {
+        std::cout << "[ERROR]: ROOT version too old! Need ROOT 6 or higher." << std::endl;
+        return -1;
+    }
+
+    std::vector<std::string> v_files;
+    std::string h1_name("sel_best_fit");
+    std::string h2_name("tru_best_fit");
+    std::string cov_name("xsec_cov");
+
+    if(is_txt_file)
+    {
+        std::ifstream fin(filename, std::ios::in);
+        if(!fin.is_open())
+        {
+            std::cout << "[ERROR]: Failed to open " << filename << std::endl;
+            return -1;
+        }
+        else
+        {
+            std::string line;
+            while(std::getline(fin, line))
+                v_files.emplace_back(line);
+        }
+    }
+    else
+        v_files.emplace_back(filename);
+
+    unsigned int dof = 0;
+    TH1D* h_chisq = new TH1D("h_chisq", "h_chisq", 25, 0, 100);
+
+    for(const auto& file : v_files)
+    {
+        std::cout << "Reading " << file << std::endl;
+        TFile* f = TFile::Open(file.c_str(), "READ");
+        TH1D* h1 = nullptr;
+        TH1D* h2 = nullptr;
+        TMatrixDSym* cov_sym = nullptr;
+
+        f->GetObject(h1_name.c_str(), h1);
+        f->GetObject(h2_name.c_str(), h2);
+        f->GetObject(cov_name.c_str(), cov_sym);
+
+        if(h1->GetNbinsX() != h2->GetNbinsX())
+        {
+            std::cout << "[ERROR]: Histograms bin numbers do not match!" << std::endl;
+            return -1;
+        }
+
+        TMatrixD cov(*cov_sym);
+        TMatrixD inv(*cov_sym);
+
+        double det = 0;
+        if(!TDecompLU::InvertLU(inv, 1E-100, &det))
+        {
+            std::cout << "[ERROR]: Failed to invert matrix." << std::endl;
+            return -1;
+        }
+
+        double chisq = 0;
+        const unsigned int nbins = cov.GetNrows();
+        for(int i = 0; i < nbins; ++i)
+        {
+            for(int j = 0; j < nbins; ++j)
+            {
+                double x = h1->GetBinContent(i+1) - h2->GetBinContent(i+1);
+                double y = h1->GetBinContent(j+1) - h2->GetBinContent(j+1);
+                chisq += x * y * inv[i][j];
+            }
+        }
+
+        if(is_txt_file)
+        {
+            dof = nbins - 1;
+            h_chisq->Fill(chisq);
+            std::cout << "Chisq = " << chisq << std::endl;
+        }
+        else
+        {
+            std::cout << "Chisq = " << chisq << std::endl;
+            return chisq;
+        }
+    }
+
+    TF1* f_chisq = new TF1("f_chisq", "ROOT::Math::chisquared_pdf(x,[0],0)",0,100);
+    f_chisq->SetParameter(0, dof);
+
+    TCanvas* c = new TCanvas("c","c",1200,900);
+    h_chisq->Scale(1.0 / h_chisq->Integral());
+    h_chisq->Draw("hist");
+    f_chisq->Draw("same");
+
+    c->Print("chisq_dist.png");
+    return 0;
+}
--- scripts/submit_fit_job.sh
+++ scripts/submit_fit_job.sh
@@ -5,33 +5,50 @@ RUNPATH="${SCRATCH}/xsllhFit"
 if [ ! -d $RUNPATH ]; then
     echo "Creating directory: $RUNPATH"
     mkdir $RUNPATH
-else
-    rm $RUNPATH/*
+#else
+#    rm $RUNPATH/*
 fi
 
 if [ -z ${1} ]; then
-    echo "No config file specified. Exiting."
+    echo "No fit config file specified. Exiting."
     exit 64
 fi
 
 if [ -z ${2} ]; then
+    echo "No errprop config file specified. Exiting."
+    exit 64
+fi
+
+if [ -z ${3} ]; then
     NUMJOBS=0
 else
-    NUMJOBS=$((${2}-1))
+    NUMJOBS=$((${3}-1))
+fi
+
+if [ -z ${4} ]; then
+    SEEDOFFSET=$RANDOM
+else
+    SEEDOFFSET=${4}
 fi
 
 THREADS=$(grep -i "num_threads" ${1} | sed 's/[^0-9]*//g')
-CONFIG=$(basename ${1})
+FITCONFIG=$(basename ${1})
+ERRCONFIG=$(basename ${2})
 
 sed -i "/#SBATCH --cpus-per-task/c\#SBATCH --cpus-per-task=${THREADS}" ${FITROOT}/scripts/fit_job.sb
 sed -i "/#SBATCH --array/c\#SBATCH --array=0-${NUMJOBS}" ${FITROOT}/scripts/fit_job.sb
-sed -i "/FITCONFIG=/c\FITCONFIG=\"${CONFIG}\"" ${FITROOT}/scripts/fit_job.sb
+sed -i "/FITCONFIG=/c\FITCONFIG=\"${FITCONFIG}\"" ${FITROOT}/scripts/fit_job.sb
+sed -i "/ERRCONFIG=/c\ERRCONFIG=\"${ERRCONFIG}\"" ${FITROOT}/scripts/fit_job.sb
+sed -i "/OFFSET=/c\OFFSET=${SEEDOFFSET}" ${FITROOT}/scripts/fit_job.sb
 
-echo "Config File: ${1}"
+echo "Fit Config File: ${1}"
+echo "Err Config File: ${2}"
 echo "Threads: ${THREADS}"
 echo "Num Jobs: $(($NUMJOBS+1))"
+echo "Seed offset: ${SEEDOFFSET}"
 
 cp ${1} $RUNPATH
+cp ${2} $RUNPATH
 cd $RUNPATH
 
 sbatch ${FITROOT}/scripts/fit_job.sb
