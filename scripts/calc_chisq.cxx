07d23fbb41a3396730ee7cfcc1e6f19e58be8240
cuddandr <cuddandr@msu.edu>
2019-10-07 09:29:50 -0400

    Updates to calc_chisq, change to build setup script.
    Updated calc_chisq.cxx to include the number of failed fits as defined
    by some chisq cutoff value. Improved visual look of plots. Adds a chisq
    distribution based on the fit to toys.
    
    Slightly edited the build script which is auto-generated by CMake to fix
    a bizarre bug seen on other platforms. Somehow when sourcing ROOT after
    editing the paths to include the fit, ROOT incorrectly changes the
    fitter paths. Sourcing ROOT first fixes this problem.
    
    It shouldn't matter at all when/where ROOT is sourced, but... this works and
    we're just going to roll with it.

--- scripts/calc_chisq.cxx
+++ scripts/calc_chisq.cxx
@@ -31,7 +31,10 @@ double calc_chisq(std::string filename, bool is_txt_file = false)
         v_files.emplace_back(filename);
 
     unsigned int dof = 0;
-    TH1D* h_chisq = new TH1D("h_chisq", "h_chisq", 50, 0, 300);
+    unsigned int num_failed_toys = 0;
+    const unsigned int num_toys = v_files.size();
+    const unsigned int CHISQ_CUTOFF = 150;
+    TH1D* h_chisq = new TH1D("h_chisq", "h_chisq", 25, 0, CHISQ_CUTOFF);
     TH2D* h_candle = new TH2D("h_candle", "h_candle", 70, 0, 70, 10000, -2E-39, 15E-39);
     //TH2D* h_errors = new TH2D("h_errors", "h_errors", 70, 0, 70, 10000, 1E-42, 5E-39);
     TH2D* h_errors = new TH2D("h_errors", "h_errors", 70, 0, 70, 1000, 0, 3);
@@ -75,9 +78,20 @@ double calc_chisq(std::string filename, bool is_txt_file = false)
                 chisq += x * y * inv[i][j];
             }
 
-            h_candle->Fill(i, h1->GetBinContent(i+1));
-            h_errors->Fill(i, h1->GetBinError(i+1)/h1->GetBinContent(i+1));
+            //h_candle->Fill(i, h1->GetBinContent(i+1));
+            //h_errors->Fill(i, h1->GetBinError(i+1)/h1->GetBinContent(i+1));
         }
+
+        if(chisq < CHISQ_CUTOFF)
+        {
+            for(int i = 0; i < nbins; ++i)
+            {
+                h_candle->Fill(i, h1->GetBinContent(i+1));
+                h_errors->Fill(i, h1->GetBinError(i+1)/h1->GetBinContent(i+1));
+            }
+        }
+        else
+            num_failed_toys += 1;
 
         if(is_txt_file)
         {
@@ -92,21 +106,53 @@ double calc_chisq(std::string filename, bool is_txt_file = false)
         }
     }
 
+    std::cout << "Chisq cutoff: " << CHISQ_CUTOFF << std::endl;
+    std::cout << "Total num. toys: " << num_toys << std::endl;
+    std::cout << "Num. failed toys: " << num_failed_toys << std::endl;
+    std::cout << "Finished calculating. Building plots." << std::endl;
+
     TF1* f_chisq = new TF1("f_chisq", "ROOT::Math::chisquared_pdf(x,[0],0)",0,150);
     f_chisq->SetParameter(0, dof);
 
+    TF1* f_fitsq = new TF1("f_fitsq", "ROOT::Math::chisquared_pdf(x,[0],0)",0,150);
+    f_fitsq->SetParameter(0, dof);
+
+    gStyle->SetOptTitle(0);
+    gStyle->SetOptFit(1);
+
     TCanvas* c = new TCanvas("c","c",1200,900);
-    h_chisq->Scale(1.0 / h_chisq->Integral());
+    h_chisq->Scale(1.0 / h_chisq->Integral(), "width");
+    h_chisq->SetLineColor(kBlack);
+    h_chisq->SetLineWidth(2);
+    h_chisq->GetXaxis()->SetTitle("#chi^{2}");
+    h_chisq->GetYaxis()->SetTitle("Normalized count");
     h_chisq->Draw("hist");
     f_chisq->Draw("same");
 
+    h_chisq->Fit(f_fitsq, "", "", 0, 150);
+    f_fitsq->SetLineColor(kBlue);
+    f_fitsq->SetLineStyle(kDashed);
+    f_fitsq->Draw("same");
+
     c->Print("chisq_dist.png");
 
+    gStyle->SetOptStat(0);
     TCanvas* v = new TCanvas("v","v",1200,900);
+    h_candle->SetLineColor(kBlack);
+    h_candle->SetLineWidth(2);
+    h_candle->GetXaxis()->SetTitle("Bin Number");
+    h_candle->GetYaxis()->SetTitle("Cross section");
     h_candle->Draw("CANDLEX3");
+    v->Print("candle_xsec.png");
 
     TCanvas* e = new TCanvas("e","e",1200,900);
+    h_errors->SetLineColor(kBlack);
+    h_errors->SetLineWidth(2);
+    h_errors->GetXaxis()->SetTitle("Bin Number");
+    h_errors->GetYaxis()->SetTitle("Relative Error");
+    //h_errors->GetYaxis()->SetRangeUser(0, 1);
     h_errors->Draw("CANDLEX3");
+    e->Print("candle_error.png");
 
     return 0;
 }
