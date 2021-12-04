6a5106b8c9e161535d19d0d0b5aa44d4c7c8989b
jaafar-chakrani <jchakrani@gmail.com>
2021-12-04 16:26:50 +0100

    Fix nan step sizes when deltaChi2<0

--- src/Fitter/src/FitterEngine.cpp
+++ src/Fitter/src/FitterEngine.cpp
@@ -1071,7 +1071,7 @@ void FitterEngine::rescaleParametersStepSize(){
         double deltaChi2Pulls = _chi2PullsBuffer_ - baseChi2Pull;
 
 //        double stepSize = TMath::Sqrt(deltaChi2Pulls)/TMath::Sqrt(deltaChi2);
-        double stepSize = 1./TMath::Sqrt(deltaChi2);
+        double stepSize = 1./TMath::Sqrt(std::fabs(deltaChi2));
 
         LogInfo << "Step size of " << parSet.getName() + "/eigen_#" << iEigen
                 << " -> σ x " << _parStepScale_ << " x " << stepSize
@@ -1105,7 +1105,7 @@ void FitterEngine::rescaleParametersStepSize(){
 //        double stepSize = TMath::Sqrt(deltaChi2Pulls)/TMath::Sqrt(deltaChi2);
 
         // full rescale
-        double stepSize = 1./TMath::Sqrt(deltaChi2);
+        double stepSize = 1./TMath::Sqrt(std::fabs(deltaChi2));
 
         LogInfo << "Step size of " << parSet.getName() + "/" + par.getTitle()
             << " -> σ x " << _parStepScale_ << " x " << stepSize
