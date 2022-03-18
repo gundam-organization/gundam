0fde9ba57300a1fb86a8857e21171448e9217079
Clark McGrew <clark.mcgrew@stonybrook.edu>
2022-03-18 18:39:56 -0400

    Add copyright licences (MIT) so people can use this

--- src/Utils/src/GPUInterpCachedWeights.cpp
+++ src/Utils/src/GPUInterpCachedWeights.cpp
@@ -676,6 +676,28 @@ void GPUInterp::CachedWeights::UpdateResults() {
     fResults->hostPtr();  // A simple way to copy from the device
 }
 
+// An MIT Style License
+
+// Copyright (c) 2022 Clark McGrew
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 // Local Variables:
 // mode:c++
 // c-basic-offset:4
