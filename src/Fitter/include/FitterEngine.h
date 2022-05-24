27872b595bef4a39ae36043a5f64af9bf3977c30
jaafar-chakrani <jchakrani@gmail.com>
2022-05-24 23:51:13 +0200

    Add option to set the rate of refreshing the monitor during the fit to be able to reduce the size of logs

--- src/Fitter/include/FitterEngine.h
+++ src/Fitter/include/FitterEngine.h
@@ -83,6 +83,8 @@ private:
   bool _fitHasConverged_{false};
   bool _isBadCovMat_{false};
 
+  long long int _monitorRefreshRateInMs_{500};
+  
   int _nbFitCalls_{0};
   int _nbFitParameters_{0};
   int _nbParameters_{0};
