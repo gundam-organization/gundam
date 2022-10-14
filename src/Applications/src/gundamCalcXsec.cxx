62a6d303e37b3d00ac7c78fd6812fec80822493b
Caspar Schloesser <caspar.maria.schloesser@cern.ch>
2022-10-14 15:37:44 +0200

    Remove unneeded options and add comments

--- src/Applications/src/gundamCalcXsec.cxx
+++ src/Applications/src/gundamCalcXsec.cxx
@@ -44,7 +44,7 @@ LoggerInit([]{
 int main(int argc, char** argv){
 
   GundamGreetings g;
-  g.setAppName("GundamFitter");
+  g.setAppName("GundamCalcXsec");
   g.hello();
 
 
@@ -53,21 +53,12 @@ int main(int argc, char** argv){
   // --------------------------
   CmdLineParser clParser;
 
-  clParser.addTriggerOption("dry-run", {"--dry-run", "-d"},"Perform the full sequence of initialization, but don't do the actual fit.");
-  clParser.addTriggerOption("generateOneSigmaPlots", {"--one-sigma"}, "Generate one sigma plots");
-  clParser.addTriggerOption("asimov", {"-a", "--asimov"}, "Use MC dataset to fill the data histograms");
-
-  clParser.addOption("cache", {"-C", "--cache-enabled"}, "Enable the event weight cache");
   clParser.addOption("configFile", {"-c", "--config-file"}, "Specify path to the fitter config file");
+  clParser.addOption("fitterOutputFile", {"-f"}, "Specify the fitter output file");
+  clParser.addOption("outputFile", {"-o", "--out-file"}, "Specify the CalcXsec output file");
   clParser.addOption("nbThreads", {"-t", "--nb-threads"}, "Specify nb of parallel threads");
-  clParser.addOption("inputFile", {"-i", "--in-file"}, "Specify the input file (fitter output file)");
-  clParser.addOption("outputFile", {"-o", "--out-file"}, "Specify the output file");
-  clParser.addOption("scanParameters", {"--scan"}, "Enable parameter scan before and after the fit");
-  clParser.addOption("toyFit", {"--toy"}, "Run a toy fit");
+  clParser.addOption("nToys", {"-n"}, "Specify number of toys");
   clParser.addOption("randomSeed", {"-s", "--seed"}, "Set random seed");
-
-  clParser.getOptionPtr("scanParameters")->setAllowEmptyValue(true); // --scan can be followed or not by the number of steps
-  clParser.getOptionPtr("toyFit")->setAllowEmptyValue(true); // --toy can be followed or not by the number of steps
 
   LogInfo << "Usage: " << std::endl;
   LogInfo << clParser.getConfigSummary() << std::endl << std::endl;
@@ -78,21 +69,7 @@ int main(int argc, char** argv){
 
   LogInfo << "Provided arguments: " << std::endl;
   LogInfo << clParser.getValueSummary() << std::endl << std::endl;
-  LogInfo << clParser.dumpConfigAsJsonStr() << std::endl;
 
-  std::string cacheDefault = "off";
-#ifdef GUNDAM_USING_CACHE_MANAGER
-  if (Cache::Manager::HasCUDA()) cacheDefault = "on";
-#endif
-  std::string cacheEnabled = clParser.getOptionVal("cache",cacheDefault);
-  if (cacheEnabled != "on") {
-      LogInfo << "Cache::Manager disabled" << std::endl;
-      GlobalVariables::setEnableCacheManager(false);
-  }
-  else {
-      LogInfo << "Enabling Cache::Manager" << std::endl;
-      GlobalVariables::setEnableCacheManager(true);
-  }
 
   if( clParser.isOptionTriggered("randomSeed") ){
     LogAlert << "Using user-specified random seed: " << clParser.getOptionVal<ULong_t>("randomSeed") << std::endl;
@@ -124,6 +101,13 @@ int main(int argc, char** argv){
     LogInfo << "Version check passed: " << GundamVersionConfig::getVersionStr() << " >= " << JsonUtils::fetchValue<std::string>(jsonConfig, "minGundamVersion") << std::endl;
   }
 
+
+
+  // Open output of the fitter
+  // Get configuration of fitter (TNamed) -> parse to json config file
+
+
+
   std::string outFileName = configFilePath;
 
   outFileName = clParser.getOptionVal("outputFile", outFileName + ".root");
@@ -140,36 +124,15 @@ int main(int argc, char** argv){
 
   // Gundam version?
   TNamed gundamVersionString("gundamVersion", GundamVersionConfig::getVersionStr().c_str());
-  GenericToolbox::writeInTFile(GenericToolbox::mkdirTFile(out, "gundamFitter"), &gundamVersionString);
+  GenericToolbox::writeInTFile(GenericToolbox::mkdirTFile(out, "gundamCalcXsec"), &gundamVersionString);
 
   // Command line?
   TNamed commandLineString("commandLine", clParser.getCommandLineString().c_str());
   GenericToolbox::writeInTFile(GenericToolbox::mkdirTFile(out, "gundamFitter"), &commandLineString);
 
-  // Config unfolded ?
-  auto unfoldedConfig = jsonConfig;
-  JsonUtils::unfoldConfig(unfoldedConfig);
-  std::stringstream ss;
-  ss << unfoldedConfig << std::endl;
-  TNamed unfoldedConfigString("unfoldedConfig", ss.str().c_str());
-  GenericToolbox::writeInTFile(GenericToolbox::mkdirTFile(out, "gundamFitter"), &unfoldedConfigString);
+  // Create a propagator object
 
-
-  LogInfo << "FitterEngine setup..." << std::endl;
-
-  // Fitter
-  FitterEngine fitter;
-  fitter.setConfig(JsonUtils::fetchSubEntry(jsonConfig, {"fitterEngineConfig"}));
-  fitter.setSaveDir(GenericToolbox::mkdirTFile(out, "FitterEngine"));
-
-  fitter.getPropagator().setLoadAsimovData( clParser.isOptionTriggered("asimov") );
-
-  fitter.initialize();
-
-  fitter.updateChi2Cache();
-  LogInfo << "Initial χ² = " << fitter.getChi2Buffer() << std::endl;
-  LogInfo << "Initial χ²(stat) = " << fitter.getChi2StatBuffer() << std::endl;
-
+  
 
 
 
@@ -187,10 +150,6 @@ int main(int argc, char** argv){
 
 
   // Get best fit parameter values and postfit covariance matrix
-  auto fFitterOutput = clParser.getOptionVal<std::string>("inputFile");
-  TFile* postfit_file = TFile::Open(fFitterOutput.c_str(), "READ");
-  TH1D* postfit_params = (TH1D*)postfit_file->Get("FitterEngine/postFit/Migrad/errors/Template Parameters/values/postFitErrors_TH1D");
-  TMatrixTSym<double>* postfit_cov = (TMatrixTSym<double>*)postfit_file->Get("FitterEngine/postFit/Migrad/errors/Template Parameters/matrices/Covariance_TMatrixD");
 
 
 
@@ -219,6 +178,7 @@ int main(int argc, char** argv){
 
 
   // Create toys which throw the fit parameters according to chol.-decomp. postfit cov matrix
+  // get number of toys from cmlParser
   for (int iToy = 0; iToy < 1000; iToy++) {
     std::vector<double> throws = GenericToolbox::throwCorrelatedParameters(choleskyMatrix);
     for (int j = 0; j < throws.size(); j++){
