3bccf7427cb16b6e5373a56432cc6c417a6178cf
Adrien Blanchet <adrien.blanchet@live.fr>
2022-10-26 13:19:35 +0200

    removing debug prints

--- src/DatasetManager/src/DataDispenser.cpp
+++ src/DatasetManager/src/DataDispenser.cpp
@@ -797,7 +797,6 @@ void DataDispenser::readAndFill(){
           // Propagate transformations for indexing
           for( auto* varTransformPtr : varTransformForIndexingList ){
             varTransformPtr->evalAndStore(eventBuffer);
-            varTransformPtr->storeCachedOutput(eventBuffer);
           }
 
           // Has valid bin?
@@ -834,28 +833,6 @@ void DataDispenser::readAndFill(){
           // Propagate transformation for storage -> use the previous results calculated for indexing
           for( auto* varTransformPtr : varTransformForStorageList ){
             varTransformPtr->storeCachedOutput(*eventPtr);
-            if( iThread_ == 0 ){
-              if( eventBuffer.getVarAsDouble("Pmu") != eventPtr->getVarAsDouble("Pmu") ){
-
-                LogTrace << "MISMATCH: "
-                         << std::endl << GET_VAR_NAME_VALUE(eventBuffer.getVarAsDouble("Pmu"))
-                         << std::endl << GET_VAR_NAME_VALUE(eventPtr->getVarAsDouble("Pmu"))
-                         << std::endl << "CC is : " << varTransformPtr->eval(eventBuffer)
-                         << std::endl;
-
-                varTransformPtr->storeCachedOutput(eventBuffer);
-
-                LogTrace << GenericToolbox::parseVectorAsString(varTransformForIndexingList) << std::endl;
-                LogTrace << GenericToolbox::parseVectorAsString(varTransformForStorageList) << std::endl;
-                LogTrace << "NOW? " << GET_VAR_NAME_VALUE(eventBuffer.getVarAsDouble("Pmu")) << std::endl;
-
-                eventBuffer.print();
-                eventPtr->print();
-
-                LogThrow("SOTP");
-              }
-
-            }
           }
 
 
