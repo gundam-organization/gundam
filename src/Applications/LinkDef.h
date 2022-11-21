4ef4b2895f7b7bd4e10acb4888bf223271f317d7
Adrien Blanchet <adrien.blanchet@live.fr>
2022-11-21 15:45:19 +0100

    adding gundamRoot interpreter, including GenericToolbox functions

---
+++ src/Applications/LinkDef.h
@@ -0,0 +1,20 @@
+//
+// Created by Adrien BLANCHET on 21/11/2022.
+//
+
+#ifndef GUNDAM_LINKDEF_H
+#define GUNDAM_LINKDEF_H
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ defined_in "GenericToolbox.Root.h";
+
+#endif
+
+
+#endif //GUNDAM_LINKDEF_H
--- src/Applications/src/gundamRoot.cxx
+++ src/Applications/src/gundamRoot.cxx
@@ -7,13 +7,20 @@
 
 #include <cstdlib>
 
+#include "Logger.h"
+
+LoggerInit([]{
+  Logger::setUserHeaderStr("[gundamRoot.cxx]");
+});
+
 TROOT root("Rint","The ROOT Interactive Interface");
 
 int main(int argc, char **argv) {
-  auto *theApp = new TRint("Rint", &argc, argv, nullptr, 0);
-  // Init Intrinsics, build all windows, and enter event loop
-  theApp->Run();
-
-  delete theApp;
+  LogInfo << "Creating ROOT interpreter..." << std::endl;
+  auto theApp = TRint("Rint", &argc, argv, nullptr, 0);
+  LogInfo << "Including GenericToolbox.Root.h..." << std::endl;
+  theApp.ProcessLine("#include \"GenericToolbox.Root.h\"");
+  LogInfo << "Running interpreter..." << std::endl;
+  theApp.Run();
   return EXIT_SUCCESS;
 }
