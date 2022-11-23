b8c683fb94221601c46a297c7970dca0ff955462
Adrien Blanchet <adrien.blanchet@live.fr>
2022-11-23 19:10:14 +0100

    gundamRoot now loads GenericToolbox without headers installed

--- src/Applications/src/gundamRoot.cxx
+++ src/Applications/src/gundamRoot.cxx
@@ -2,6 +2,7 @@
 // Created by Adrien BLANCHET on 23/10/2022.
 //
 #include "Logger.h"
+#include "GenericToolbox.TablePrinter.h"
 
 #include "TRint.h"
 
@@ -25,22 +26,26 @@ int main(int argc, char **argv) {
   cstrings.reserve(argvVector.size());
   for(auto& s: argvVector) cstrings.push_back(&s[0]);
   int argcInterpreter = int( cstrings.size() );
-
   char** argvInterpreter{ cstrings.data() };
+
+  GenericToolbox::TablePrinter t;
 
   LogInfo << "Creating ROOT interpreter..." << std::endl;
   auto *theApp = new TRint(
-      "Rint",
-      &argcInterpreter,
-      argvInterpreter,
-      /*options*/ nullptr,
-      /*numOptions*/ 0,
-      /*noLogo*/ kFALSE
-//    , /*exitOnUnknownArgs*/ kTRUE // ROOT 6.20?
+      "Rint"
+      ,&argcInterpreter
+      ,argvInterpreter
+      , nullptr/*options*/
+      , 0 /*numOptions*/
+      , kFALSE /*noLogo*/
+//    , kTRUE /*exitOnUnknownArgs*/ // ROOT 6.20?
   );
 
-//  LogInfo << "Including GenericToolbox.Root.h..." << std::endl;
+  LogInfo << "Enabling GenericToolbox lib..." << std::endl;
 //  theApp->ProcessLine("#include \"GenericToolbox.Root.h\"");
+  theApp->ProcessLine("{ GenericToolbox::TablePrinter t; }");
+
+  theApp->SetPrompt("gundamRoot [%d] ");
 
   LogInfo << "Running interpreter..." << std::endl;
   theApp->Run();
