e3676eda6ad3f8f7bb9faca968b292e06f94d914
Clark McGrew <clark.mcgrew@stonybrook.edu>
2023-02-01 14:19:06 -0500

    Generate an MCMC with highly correlated parameters
    The posterior for the two parameters should be uniformly
    distributed between [0,2].  The sum should be tighly fixed at
    1.0.

---
+++ tests/slow-tests/200HorrifyingMCMC.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Set the base name for this test (should match the script name)
+BASE=200HorrifyingMCMC
+
+# Get the directory containing the script from the command line
+# parameters (avoids bash trickery).  Use the current directory as the
+# default.
+DIR=.
+if [ ${#1} -gt 0 ]; then
+    DIR=${1}
+fi
+
+# Make sure that gundam has been setup.
+if ! which gundamFitter; then
+    echo FAIL: Executable not found for gundamFitter
+    exit 1
+fi
+
+# Set the expected locations for the config and output files.
+export CONFIG_DIR=${DIR}
+export DATA_DIR=${PWD}
+
+CONFIG_FILE=${CONFIG_DIR}/${BASE}-config.yaml
+OUTPUT_FILE=${DATA_DIR}/${BASE}.root
+
+echo ${OUTPUT_FILE}
+echo ${CONFIG_FILE}
+
+gundamFitter -t 1 -s 10000 -c ${CONFIG_FILE} -o ${OUTPUT_FILE}
+
+# End of the script
