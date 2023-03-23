a70358218e2d9ab483cc0ad25b99633249745535
Clark McGrew <clark.mcgrew@stonybrook.edu>
2023-03-23 16:55:42 -0400

    Migrate MonotonicSplineHandler methods to MonotonicSpline
    This changes to single inheritance from DialBase.  The visible
    external interface is unchanged, and the code is a direct copy.  The
    changes to CMakeLists.txt will be in a later commit.

--- src/DialDictionary/include/MonotonicSpline.h
+++ src/DialDictionary/include/MonotonicSpline.h
@@ -6,17 +6,39 @@
 #define GUNDAM_MONOTONICSPLINE_H
 
 #include "DialBase.h"
-#include "MonotonicSplineHandler.h"
+#include "DialInputBuffer.h"
 
-class MonotonicSpline : public DialBase, public MonotonicSplineHandler {
+#include "TGraph.h"
+
+#include "vector"
+#include "utility"
+
+class MonotonicSpline : public DialBase {
 
 public:
   MonotonicSpline() = default;
+  virtual ~MonotonicSpline() = default;
 
   [[nodiscard]] std::unique_ptr<DialBase> clone() const override { return std::make_unique<MonotonicSpline>(*this); }
   [[nodiscard]] std::string getDialTypeName() const override { return {"MonotonicSpline"}; }
   double evalResponseImpl(const DialInputBuffer& input_) override { return this->evaluateSpline(input_); }
 
+  void setAllowExtrapolation(bool allowExtrapolation);
+
+  void buildSplineData(TGraph& graph_);
+  [[nodiscard]] double evaluateSpline(const DialInputBuffer& input_) const;
+
+  bool getIsUniform() const {return true;}
+  const std::vector<double>& getSplineData() const {return _splineData_;}
+
+protected:
+  bool _allowExtrapolation_{false};
+
+  // A block of data to calculate the spline values.  This must be filled for
+  // the Cache::Manager to work, and provides the input for spline calculation
+  // functions that can be shared between the CPU and the GPU.
+  std::vector<double> _splineData_{};
+  std::pair<double, double> _splineBounds_{std::nan("unset"), std::nan("unset")};
 };
 
 
