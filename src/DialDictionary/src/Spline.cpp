8bd47d1754f63a95f8e997f776389ae0fab62d04
Clark McGrew <clark.mcgrew@stonybrook.edu>
2023-03-23 16:59:19 -0400

    Migrate SplineHandler methods to Spline
    This changes to single inheritance from DialBase.  The visible
    external interface is unchanged, and the code is a direct copy.  The
    changes to CMakeLists.txt will be in a later commit.

--- src/DialDictionary/src/Spline.cpp
+++ src/DialDictionary/src/Spline.cpp
@@ -2,27 +2,25 @@
 // Created by Adrien Blanchet on 30/11/2022.
 //
 
-#include "SplineHandler.h"
+#include "Spline.h"
 
-
-
-void SplineHandler::setAllowExtrapolation(bool allowExtrapolation) {
+void Spline::setAllowExtrapolation(bool allowExtrapolation) {
   _allowExtrapolation_ = allowExtrapolation;
 }
-void SplineHandler::setSpline(const TSpline3 &spline) {
+void Spline::setSpline(const TSpline3 &spline) {
   _spline_ = spline;
 }
 
-const TSpline3 &SplineHandler::getSpline() const {
+const TSpline3 &Spline::getSpline() const {
   return _spline_;
 }
 
-void SplineHandler::copySpline(const TSpline3* splinePtr_){
+void Spline::copySpline(const TSpline3* splinePtr_){
   // Don't check for override: when loading toy + mc data, these placeholders has to be filled up twice
 //  LogThrowIf(_graph_.GetXmin() != _graph_.GetXmax(), "Spline already set");
   _spline_ = *splinePtr_;
 }
-void SplineHandler::createSpline(TGraph* grPtr_){
+void Spline::createSpline(TGraph* grPtr_){
 //  LogThrowIf(_graph_.GetXmin() != _graph_.GetXmax(), "Spline already set");
   _spline_ = TSpline3(grPtr_->GetName(), grPtr_);
 #ifdef ENABLE_SPLINE_DIAL_FAST_EVAL
@@ -30,7 +28,7 @@ void SplineHandler::createSpline(TGraph* grPtr_){
 #endif
 }
 
-double SplineHandler::evaluateSpline(const DialInputBuffer& input_) const{
+double Spline::evaluateSpline(const DialInputBuffer& input_) const{
   if( not _allowExtrapolation_ ){
     if     (input_.getBuffer()[0] <= _spline_.GetXmin()) { return _spline_.Eval( _spline_.GetXmin() ); }
     else if(input_.getBuffer()[0] >= _spline_.GetXmax()) { return _spline_.Eval( _spline_.GetXmax() ); }
