1a04140adf11234732e58bb67a580c893b5f94af
Clark McGrew <clark.mcgrew@stonybrook.edu>
2023-03-23 16:53:32 -0400

    Migrate LightGraphHandler methods to LightGraph
    This changes to single inheritance from DialBase.  The visible
    external interface is unchanged, and the code is a direct copy.  The
    changes to CMakeLists.txt will be in a later commit.

--- src/DialDictionary/include/LightGraph.h
+++ src/DialDictionary/include/LightGraph.h
@@ -6,18 +6,29 @@
 #define GUNDAM_LIGHTGRAPH_H
 
 #include "DialBase.h"
-#include "LightGraphHandler.h"
+#include "TGraph.h"
+#include "vector"
 
-class LightGraph : public DialBase, public LightGraphHandler {
 
-  public:
-  LightGraph() = default;
+class LightGraph : public DialBase {
 
+public:
+    LightGraph() = default;
+     
     [[nodiscard]] std::unique_ptr<DialBase> clone() const override { return std::make_unique<LightGraph>(*this); }
     [[nodiscard]] std::string getDialTypeName() const override { return {"LightGraph"}; }
     double evalResponseImpl(const DialInputBuffer& input_) override { return this->evaluateGraph(input_); }
 
-};
+    void setAllowExtrapolation(bool allowExtrapolation);
+  void setGraph(TGraph &graph);
 
+  [[nodiscard]] double evaluateGraph(const DialInputBuffer& input_) const;
+
+protected:
+  bool _allowExtrapolation_{false};
+  Int_t nPoints{0};   ///< Number of points <= fMaxSize
+  std::vector<Double_t> xPoints; ///<[fNpoints] array of X points
+  std::vector<Double_t> yPoints; ///<[fNpoints] array of Y points
+};
 
 #endif //GUNDAM_LIGHTGRAPH_H
