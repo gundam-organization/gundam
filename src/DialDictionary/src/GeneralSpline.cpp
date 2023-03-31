b00d8c582c6a07f7225e34161b382498e7096c40
Clark McGrew <clark.mcgrew@stonybrook.edu>
2023-03-31 18:08:38 -0400

    Update GeneralSpline to use base class methods

--- src/DialDictionary/src/GeneralSpline.cpp
+++ src/DialDictionary/src/GeneralSpline.cpp
@@ -17,34 +17,62 @@ void GeneralSpline::setAllowExtrapolation(bool allowExtrapolation) {
   _allowExtrapolation_ = allowExtrapolation;
 }
 
-void GeneralSpline::buildSplineData(TGraph& graph_){
-  // Copy the spline data into local storage.
-  graph_.Sort();
-  buildSplineData(TSpline3(Form("%p", &graph_), &graph_));
+bool GeneralSpline::getAllowExtrapolation() const {
+  return _allowExtrapolation_;
 }
-void GeneralSpline::buildSplineData(const TSpline3& sp_){
-  LogThrowIf(not _splineData_.empty(), "Spline data already set.");
 
-  _splineBounds_.first = sp_.GetXmin();
-  _splineBounds_.second = sp_.GetXmax();
+void GeneralSpline::buildDial(const TGraph& graph_, std::string option_){
+  // Copy the spline data into local storage.
+  TGraph grf(graph_);
+  grf.Sort();
+  buildDial(TSpline3(Form("%p", &graph_), &graph_), option_);
+}
 
-  _splineData_.resize(2 + sp_.GetNp()*3);
-  _splineData_[0] = sp_.GetXmin();
-  _splineData_[1] = ( ( sp_.GetXmax() - sp_.GetXmin() ) / ( sp_.GetNp()-1.0 ) );
+void GeneralSpline::buildDial(const TSpline3& sp_, std::string option_){
+  std::vector<double> xPoints;
+  std::vector<double> yPoints;
+  std::vector<double> deriv;
 
   // Copy the spline data into local storage.
   for (int i = 0; i < sp_.GetNp(); ++i) {
     double x;
     double y;
     sp_.GetKnot(i,x,y);
+    double d = sp_.Derivative(x);
+    xPoints.push_back(x);
+    yPoints.push_back(y);
+    deriv.push_back(d);
+  }
+  buildDial(xPoints, yPoints, deriv);
+}
+
+void GeneralSpline::buildDial(const std::vector<double>& xPoints,
+                              const std::vector<double>& yPoints,
+                              const std::vector<double>& deriv,
+                              std::string option_){
+  LogThrowIf(not _splineData_.empty(), "Spline data already set.");
+
+  _splineBounds_.first = xPoints.front();
+  _splineBounds_.second = xPoints.back();
+
+  _splineData_.resize(2 + xPoints.size()*3);
+  _splineData_[0] = xPoints.front();
+  _splineData_[1] = (xPoints.back()-xPoints.front())/(xPoints.size()-1.0);
+
+  // Copy the spline data into local storage.
+  for (int i = 0; i < xPoints.size(); ++i) {
+    double x = xPoints[i];
+    double y = yPoints[i];
+    double d = deriv[i];
     _splineData_[2 + 3*i + 0] = y;
-    _splineData_[2 + 3*i + 1] = sp_.Derivative(x);
+    _splineData_[2 + 3*i + 1] = d;
     _splineData_[2 + 3*i + 2] = x;
   }
 
 //  LogThrow(GenericToolbox::parseVectorAsString(_splineData_));
 }
-double GeneralSpline::evaluateSpline(const DialInputBuffer& input_) const{
+
+double GeneralSpline::evalResponse(const DialInputBuffer& input_) const {
   double dialInput{input_.getBuffer()[0]};
 
   if( not _allowExtrapolation_ ){
