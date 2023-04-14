f02c0099f10d8d5186b4a6be777e765fa4a7b2c5
Clark McGrew <clark.mcgrew@stonybrook.edu>
2023-04-14 14:11:46 -0400

    Fix documentation to match code in DialBaseFactory.h
    The method naming was updated to make life easier on older IDEs (avoid
    using operator()), but the documentation was still calling the class a
    functor.  This fixes the tension.

--- src/DialDictionary/DialFactories/include/DialBaseFactory.h
+++ src/DialDictionary/DialFactories/include/DialBaseFactory.h
@@ -7,8 +7,14 @@
 
 #include <string>
 
-// A functor class that will build DialBase objects and return the pointer to
-// the object.  The ownership of the object is passed to the caller.
+// A factory that will build DialBase objects and return the pointer to the
+// object.  It's written as a class instead of as a function in case we want
+// to close around values, and the normal implemenation would be as a functor,
+// but that doesn't work well in some IDEs.  This will use other factories to
+// build the dials, so try to keep the interaces uniform (there isn't tight
+// inheritance relationship, and C++ isn't good at pure "interface" classes).
+// The ownership of the object is passed to the caller so the pointer should
+// be put into a managed pointer (e.g. unique_ptr, or shared_ptr).
 class DialBaseFactory {
 public:
   DialBaseFactory() = default;
@@ -16,13 +22,14 @@ public:
 
   // Construct a pointer to the correct DialBase.  This uses the dialType and
   // dialSubType to figure out the correct class, and then uses the object
-  // pointed to by the dialInitializer to fill the dial.  The ownership of the
+  // pointed to by the dialInitializer to fill the dial.  The initializer is
+  // not a constant due to constraints from ROOT.  NOTE: The ownership of the
   // pointer is passed to the caller, so it should be put in a managed
   // variable (e.g. a unique_ptr, or shared_ptr).
   DialBase* makeDial(const std::string& dialType_,
-                         const std::string& dialSubType_,
-                         TObject* dialInitializer_,
-                         bool useCachedDial_);
+                     const std::string& dialSubType_,
+                     TObject* dialInitializer_,
+                     bool useCachedDial_);
 };
 
 //  A Lesser GNU Public License
