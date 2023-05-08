ace018171f5a001ccc36792f185a174df6b64ac7
Adrien Blanchet <adrien.blanchet@live.fr>
2023-05-08 14:50:10 +0200

    debug printout

--- src/DatasetManager/src/DataDispenser.cpp
+++ src/DatasetManager/src/DataDispenser.cpp
@@ -860,14 +860,19 @@ void DataDispenser::loadFromHistContent(){
   // non-trivial as we need to propagate systematics. Need to merge with the original data loader, but not straight forward?
   LogThrowIf( _parameters_.useMcContainer, "Hist loader not implemented for MC containers" );
 
+  LogDebug << __LINE__ << std::endl;
   // counting events
   _cache_.sampleNbOfEvents.resize(_cache_.samplesToFillList.size());
 
+  LogDebug << __LINE__ << std::endl;
   PhysicsEvent eventPlaceholder;
   eventPlaceholder.setDataSetIndex(_owner_->getDataSetIndex());
 
+  LogDebug << __LINE__ << std::endl;
   // claiming event memory
   for( size_t iSample = 0 ; iSample < _cache_.sampleNbOfEvents.size() ; iSample++ ){
+
+    LogDebug << __LINE__ << GET_VAR_NAME_VALUE(iSample) << std::endl;
     // one event per bin
     _cache_.sampleNbOfEvents[iSample] = _cache_.samplesToFillList[iSample]->getBinning().getBinsList().size();
 
