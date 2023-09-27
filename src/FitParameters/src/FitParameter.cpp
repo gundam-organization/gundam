43682840b5b8fcc4c3c9df372a015a7aefc2f449
Adrien Blanchet <adrien.blanchet@live.fr>
2023-09-27 23:56:37 +0200

    cleaning up

--- src/FitParameters/src/FitParameter.cpp
+++ src/FitParameters/src/FitParameter.cpp
@@ -13,7 +13,6 @@
 
 LoggerInit([]{ Logger::setUserHeaderStr("[FitParameter]"); });
 
-FitParameter::FitParameter(const FitParameterSet* owner_): _owner_(owner_) {}
 
 void FitParameter::readConfigImpl(){
   if( not _parameterConfig_.empty() ){
@@ -58,7 +57,6 @@ void FitParameter::readConfigImpl(){
     _dialDefinitionsList_ = GenericToolbox::Json::fetchValue(_parameterConfig_, "dialSetDefinitions", _dialDefinitionsList_);
   }
 }
-
 void FitParameter::initializeImpl() {
   LogThrowIf(_owner_ == nullptr, "Parameter set ref is not set.");
   LogThrowIf(_parameterIndex_ == -1, "Parameter index is not set.");
@@ -69,61 +67,6 @@ void FitParameter::initializeImpl() {
   LogThrowIf(std::isnan(_parameterValue_), "Parameter value is not set.");
 }
 
-void FitParameter::setIsEnabled(bool isEnabled){
-  _isEnabled_ = isEnabled;
-}
-void FitParameter::setIsFixed(bool isFixed) {
-  _isFixed_ = isFixed;
-}
-void FitParameter::setIsEigen(bool isEigen) {
-  _isEigen_ = isEigen;
-}
-void FitParameter::setIsFree(bool isFree) {
-  _isFree_ = isFree;
-}
-
-void FitParameter::setDialSetConfig(const nlohmann::json &jsonConfig_) {
-  auto jsonConfig = jsonConfig_;
-  while( jsonConfig.is_string() ){
-    LogWarning << "Forwarding FitParameterSet config to: \"" << jsonConfig.get<std::string>() << "\"..." << std::endl;
-    jsonConfig = ConfigUtils::readConfigFile(jsonConfig.get<std::string>());
-  }
-  _dialDefinitionsList_ = jsonConfig.get<std::vector<nlohmann::json>>();
-}
-
-void FitParameter::setParameterDefinitionConfig(const nlohmann::json &config_){
-  _parameterConfig_ = config_;
-  ConfigUtils::forwardConfig(_parameterConfig_);
-}
-void FitParameter::setParameterIndex(int parameterIndex) {
-  _parameterIndex_ = parameterIndex;
-}
-void FitParameter::setName(const std::string &name) {
-  _name_ = name;
-}
-void FitParameter::setParameterValue(double parameterValue) {
-  LogThrowIf( std::isnan(parameterValue), "Attempting to set NaN value for par:" << std::endl << this->getSummary() );
-  if( _parameterValue_ != parameterValue ){
-    _gotUpdated_ = true;
-    _parameterValue_ = parameterValue;
-  }
-  else{ _gotUpdated_ = false; }
-}
-void FitParameter::setPriorValue(double priorValue) {
-  _priorValue_ = priorValue;
-}
-void FitParameter::setThrowValue(double throwValue){
-  _throwValue_ = throwValue;
-}
-void FitParameter::setStdDevValue(double stdDevValue) {
-  _stdDevValue_ = stdDevValue;
-}
-void FitParameter::setMinValue(double minValue) {
-  _minValue_ = minValue;
-}
-void FitParameter::setMaxValue(double maxValue) {
-  _maxValue_ = maxValue;
-}
 void FitParameter::setMinMirror(double minMirror) {
   if (std::isfinite(_minMirror_) and std::abs(_minMirror_-minMirror) > 1E-6) {
     LogWarning << "Minimum mirror bound changed for " << getFullTitle()
@@ -142,20 +85,25 @@ void FitParameter::setMaxMirror(double maxMirror) {
   }
   _maxMirror_ = maxMirror;
 }
-void FitParameter::setMinPhysical(double minPhysical) {
-  _minPhysical_ = minPhysical;
+void FitParameter::setParameterValue(double parameterValue) {
+  LogThrowIf( std::isnan(parameterValue), "Attempting to set NaN value for par:" << std::endl << this->getSummary() );
+  if( _parameterValue_ != parameterValue ){
+    _gotUpdated_ = true;
+    _parameterValue_ = parameterValue;
+  }
+  else{ _gotUpdated_ = false; }
 }
-void FitParameter::setMaxPhysical(double maxPhysical) {
-  _maxPhysical_ = maxPhysical;
+void FitParameter::setDialSetConfig(const nlohmann::json &jsonConfig_) {
+  auto jsonConfig = jsonConfig_;
+  while( jsonConfig.is_string() ){
+    LogWarning << "Forwarding FitParameterSet config to: \"" << jsonConfig.get<std::string>() << "\"..." << std::endl;
+    jsonConfig = ConfigUtils::readConfigFile(jsonConfig.get<std::string>());
+  }
+  _dialDefinitionsList_ = jsonConfig.get<std::vector<nlohmann::json>>();
 }
-void FitParameter::setStepSize(double stepSize) {
-  _stepSize_ = stepSize;
-}
-void FitParameter::setOwner(const FitParameterSet* owner_) {
-  _owner_ = owner_;
-}
-void FitParameter::setPriorType(PriorType::PriorType priorType) {
-  _priorType_ = priorType;
+void FitParameter::setParameterDefinitionConfig(const nlohmann::json &config_){
+  _parameterConfig_ = config_;
+  ConfigUtils::forwardConfig(_parameterConfig_);
 }
 
 void FitParameter::setValueAtPrior(){
@@ -163,67 +111,6 @@ void FitParameter::setValueAtPrior(){
 }
 void FitParameter::setCurrentValueAsPrior(){
   _priorValue_ = _parameterValue_;
-}
-
-bool FitParameter::isFree() const {
-  return _isFree_;
-}
-bool FitParameter::isEigen() const {
-  return _isEigen_;
-}
-bool FitParameter::isFixed() const {
-  return _isFixed_;
-}
-bool FitParameter::isEnabled() const {
-  return _isEnabled_;
-}
-int FitParameter::getParameterIndex() const {
-  return _parameterIndex_;
-}
-double FitParameter::getMinValue() const {
-  return _minValue_;
-}
-double FitParameter::getMaxValue() const {
-  return _maxValue_;
-}
-double FitParameter::getMinMirror() const {
-  return _minMirror_;
-}
-double FitParameter::getMaxMirror() const {
-  return _maxMirror_;
-}
-double FitParameter::getMinPhysical() const {
-  return _minPhysical_;
-}
-double FitParameter::getMaxPhysical() const {
-  return _maxPhysical_;
-}
-double FitParameter::getStepSize() const {
-  return _stepSize_;
-}
-double FitParameter::getPriorValue() const {
-  return _priorValue_;
-}
-double FitParameter::getThrowValue() const{
-  return _throwValue_;
-}
-double FitParameter::getStdDevValue() const {
-  return _stdDevValue_;
-}
-double FitParameter::getParameterValue() const {
-  return _parameterValue_;
-}
-const std::string &FitParameter::getName() const {
-  return _name_;
-}
-const nlohmann::json &FitParameter::getDialDefinitionsList() const {
-  return _dialDefinitionsList_;
-}
-const FitParameterSet *FitParameter::getOwner() const {
-  return _owner_;
-}
-PriorType::PriorType FitParameter::getPriorType() const {
-  return _priorType_;
 }
 
 bool FitParameter::isValueWithinBounds() const{
@@ -234,18 +121,15 @@ bool FitParameter::isValueWithinBounds() const{
 double FitParameter::getDistanceFromNominal() const{
   return (_parameterValue_ - _priorValue_) / _stdDevValue_;
 }
-
 std::string FitParameter::getTitle() const {
   std::stringstream ss;
   ss << "#" << _parameterIndex_;
   if( not _name_.empty() ) ss << "_" << _name_;
   return ss.str();
 }
-
 std::string FitParameter::getFullTitle() const{
   return _owner_->getName() + "/" + this->getTitle();
 }
-
 std::string FitParameter::getSummary(bool shallow_) const {
   std::stringstream ss;
 
