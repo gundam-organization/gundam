da80894ef591db6d2b3d0f8fa2f2157f441da5fd
Adrien Blanchet <adrien.blanchet@live.fr>
2023-10-28 00:54:05 +0200

    issue was already fixed..

--- src/DatasetManager/src/DatasetLoader.cpp
+++ src/DatasetManager/src/DatasetLoader.cpp
@@ -28,25 +28,19 @@ void DatasetLoader::readConfigImpl() {
   _isEnabled_ = GenericToolbox::Json::fetchValue(_config_, "isEnabled", true);
   LogReturnIf(not _isEnabled_, "\"" << _name_ << "\" is disabled.");
 
-  LogDebug << __LINE__ << std::endl;
-
   _selectedDataEntry_ = GenericToolbox::Json::fetchValue<std::string>(_config_, "selectedDataEntry", "Asimov");
   _selectedToyEntry_ = GenericToolbox::Json::fetchValue<std::string>(_config_, "selectedToyEntry", "Asimov");
 
-  LogDebug << __LINE__ << std::endl;
   _showSelectedEventCount_ = GenericToolbox::Json::fetchValue(_config_, "showSelectedEventCount", _showSelectedEventCount_);
 
-  LogDebug << __LINE__ << std::endl;
   _mcDispenser_ = DataDispenser(this);
   _mcDispenser_.readConfig(GenericToolbox::Json::fetchValue<nlohmann::json>(_config_, "mc"));
   _mcDispenser_.getParameters().name = "Asimov";
   _mcDispenser_.getParameters().useMcContainer = true;
 
-  LogDebug << __LINE__ << std::endl;
   // Always loaded by default
   _dataDispenserDict_.emplace("Asimov", DataDispenser(_mcDispenser_));
 
-  LogDebug << __LINE__ << std::endl;
   for( auto& dataEntry : GenericToolbox::Json::fetchValue(_config_, "data", nlohmann::json()) ){
     std::string name = GenericToolbox::Json::fetchValue(dataEntry, "name", "data");
     LogThrowIf( GenericToolbox::doesKeyIsInMap(name, _dataDispenserDict_),
@@ -57,12 +51,10 @@ void DatasetLoader::readConfigImpl() {
     _dataDispenserDict_.at(name).readConfig(dataEntry);
   }
 
-  LogDebug << __LINE__ << std::endl;
   _devSingleThreadEventLoaderAndIndexer_ = GenericToolbox::Json::fetchValue(_config_, "devSingleThreadEventLoaderAndIndexer", _devSingleThreadEventLoaderAndIndexer_);
   _devSingleThreadEventSelection_ = GenericToolbox::Json::fetchValue(_config_, "devSingleThreadEventSelection", _devSingleThreadEventSelection_);
   _sortLoadedEvents_ = GenericToolbox::Json::fetchValue(_config_, "sortLoadedEvents", _sortLoadedEvents_);
 
-  LogDebug << __LINE__ << std::endl;
 }
 void DatasetLoader::initializeImpl() {
   if( not _isEnabled_ ) return;
