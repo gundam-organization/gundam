239f2b568e0e25242b7e3e18c9db74c515423406
Adrien Blanchet <adrien.blanchet@live.fr>
2023-12-02 20:28:33 +0100

    reversing JSON/ROOT find

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -4,25 +4,6 @@ message("")
 cmessage( WARNING "Checking dependencies...")
 
 
-cmessage( STATUS "Looking for JSON install..." )
-find_package( nlohmann_json QUIET )
-
-if( nlohmann_json_FOUND )
-  cmessage( STATUS "nlohmann JSON library found.")
-  link_libraries( nlohmann_json::nlohmann_json )
-else()
-  cmessage( ALERT "nlohmann JSON library not found. Using fetch content... (CMake version >= 3.11)")
-  cmake_minimum_required( VERSION 3.11 FATAL_ERROR )
-  include( FetchContent )
-
-  FetchContent_Declare( json URL https://github.com/nlohmann/json/releases/download/v3.11.3/json.tar.xz )
-  FetchContent_MakeAvailable( json )
-
-  include_directories( ${nlohmann_json_SOURCE_DIR}/include )
-  cmessage( STATUS "nlohmann JSON library fetched: ${nlohmann_json_SOURCE_DIR}/include")
-endif()
-
-
 ##########
 # ROOT
 ##########
@@ -117,6 +98,23 @@ endif(NOT ROOT_minuit2_FOUND)
 # NLOHMANN JSON
 ####################
 
+cmessage( STATUS "Looking for JSON install..." )
+find_package( nlohmann_json QUIET )
+
+if( nlohmann_json_FOUND )
+  cmessage( STATUS "nlohmann JSON library found.")
+  link_libraries( nlohmann_json::nlohmann_json )
+else()
+  cmessage( ALERT "nlohmann JSON library not found. Using fetch content... (CMake version >= 3.11)")
+  cmake_minimum_required( VERSION 3.11 FATAL_ERROR )
+  include( FetchContent )
+
+  FetchContent_Declare( json URL https://github.com/nlohmann/json/releases/download/v3.11.3/json.tar.xz )
+  FetchContent_MakeAvailable( json )
+
+  include_directories( ${nlohmann_json_SOURCE_DIR}/include )
+  cmessage( STATUS "nlohmann JSON library fetched: ${nlohmann_json_SOURCE_DIR}/include")
+endif()
 
 
 ####################
