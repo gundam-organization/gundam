dc363f88dd28accf4d0fbe0825de53402061155a
Adrien Blanchet <adrien.blanchet@live.fr>
2023-12-02 19:48:16 +0100

    adding CTest cmake module

--- cmake/applications.cmake
+++ cmake/applications.cmake
@@ -3,6 +3,9 @@ message("")
 cmessage( WARNING "Defining applications...")
 
 add_subdirectory( ${CMAKE_SOURCE_DIR}/src/Applications )
+
+
+include( CTest )
 add_subdirectory( ${CMAKE_SOURCE_DIR}/tests )
 
 configure_file(cmake/generated/build_setup.sh.in
