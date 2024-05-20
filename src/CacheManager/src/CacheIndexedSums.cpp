4278e3d70f5015b0da1834d0338dde01854b2722
Clark McGrew <clark.mcgrew@stonybrook.edu>
2024-05-20 09:36:07 -0400

    Check validity with not isnan instead of isfinite

--- src/CacheManager/src/CacheIndexedSums.cpp
+++ src/CacheManager/src/CacheIndexedSums.cpp
@@ -86,10 +86,10 @@ double Cache::IndexedSums::GetSum(int i) {
     LogThrowIf(i<0, "Sum index out of range");
     LogThrowIf((fSums->size() <= i), "Sum index out of range");
     // This odd ordering is to make sure the thread-safe hostPtr update
-    // finishes before the sum is set to be valid.  The use of isfinite is to
+    // finishes before the sum is set to be valid.  The use of isnan is to
     // make sure that the optimizer doesn't reorder the statements.
     double value = fSums->hostPtr()[i];
-    if (std::isfinite(value)) fSumsValid = true;
+    if (not std::isnan(value)) fSumsValid = true;
     return value;
 }
 
@@ -100,7 +100,7 @@ double Cache::IndexedSums::GetSum2(int i) {
     // finishes before the sum is set to be valid.  The use of isfinite is to
     // make sure that the optimizer doesn't reorder the statements.
     double value = fSums2->hostPtr()[i];
-    if (std::isfinite(value)) fSumsValid = true;
+    if (not std::isnan(value)) fSumsValid = true;
     return value;
 }
 
