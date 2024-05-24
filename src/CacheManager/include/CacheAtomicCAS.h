99c2082d06292c25d377fcf0e4d546813ef3ac12
Clark McGrew <clark.mcgrew@stonybrook.edu>
2024-05-24 14:09:35 -0400

    Change implementation of CacheAtomicCAS to C++

--- src/CacheManager/include/CacheAtomicCAS.h
+++ src/CacheManager/include/CacheAtomicCAS.h
@@ -2,21 +2,13 @@
 #define CacheAtomicCAS_h_SEEN
 
 // Implement a "portable" atomic compare-and-set for various native types.
-// The implentation is done using C, and usually leverages the C stdatomic
-// definitions, but might depend on compiler built-in methods.  This hides the
-// details.  The semantics are similar to the C11
-// atomic_compare_exchange_weak_explicit function.  If the value pointed to by
-// "variable" is equal to the value pointed to by "expected", then set the value
-// pointed to by "variable" to "update".
-#ifdef __cplusplus
-extern "C" {
-#endif
-int CacheAtomicCAS_double(double *variable, double *expected, double update);
-int CacheAtomicCAS_float(float *variable, float *expected, float update);
-int CacheAtomicCAS_int(int *variable, int *expected, int update);
-#ifdef __cplusplus
-}
-#endif
-
+// This hides the details, and might depend on compiler built-in methods..
+// The semantics are similar to the C11 atomic_compare_exchange_weak_explicit
+// function.  If the value pointed to by "variable" is equal to the value
+// pointed to by "expected", then set the value pointed to by "variable" to
+// "update".
+int CacheAtomicCAS(double *variable, double *expected, double update);
+int CacheAtomicCAS(float *variable, float *expected, float update);
+int CacheAtomicCAS(int *variable, int *expected, int update);
 
 #endif
