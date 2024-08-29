26b2943d40b7f307a1b815c11c7ee76557465927
Adrien Blanchet <adrien.blanchet@live.fr>
2024-08-29 13:20:53 +0200

    reminding build type

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -7,7 +7,7 @@ cmessage( STATUS "Pre-checking GUNDAM version" )
 set(GENERATE_DIR_FOR_VERSION_CHECK ${CMAKE_BINARY_DIR} CACHE STRING "GENERATE_DIR_FOR_VERSION_CHECK")
 add_custom_target( preBuildVersionCheck
     COMMAND ${CMAKE_COMMAND} -D GENERATE_DIR_FOR_VERSION_CHECK=${GENERATE_DIR_FOR_VERSION_CHECK} -P ${CMAKE_SOURCE_DIR}/cmake/utils/git-version.cmake
-    COMMENT "Checking GUNDAM version with git files..."
+    COMMENT "Building GUNDAM binaries in ${CMAKE_BUILD_TYPE} mode. Checking version with git files..."
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 )
 
