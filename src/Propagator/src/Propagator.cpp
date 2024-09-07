aa93096f1a682b5425809265fccb8633e9c7a6d1
Adrien Blanchet <adrien.blanchet@live.fr>
2024-09-07 10:32:10 +0200

    adding _showNbEventParameterBreakdown_ and making _showStagedEventBreakdown_ optional

--- src/Propagator/src/Propagator.cpp
+++ src/Propagator/src/Propagator.cpp
@@ -44,7 +44,9 @@ void Propagator::readConfigImpl(){
 
   // Monitoring parameters
   _showEventBreakdown_ = GenericToolbox::Json::fetchValue(_config_, "showEventBreakdown", _showEventBreakdown_);
+  _showStagedEventBreakdown_ = GenericToolbox::Json::fetchValue(_config_, "showStagedEventBreakdown", _showStagedEventBreakdown_);
   _showNbEventParameterBreakdown_ = GenericToolbox::Json::fetchValue(_config_, "showNbEventParameterBreakdown", _showNbEventParameterBreakdown_);
+  _showNbEventPerSampleParameterBreakdown_ = GenericToolbox::Json::fetchValue(_config_, "showNbEventPerSampleParameterBreakdown", _showNbEventPerSampleParameterBreakdown_);
   _throwAsimovToyParameters_ = GenericToolbox::Json::fetchValue(_config_, "throwAsimovFitParameters", _throwAsimovToyParameters_);
   _enableStatThrowInToys_ = GenericToolbox::Json::fetchValue(_config_, "enableStatThrowInToys", _enableStatThrowInToys_);
   _gaussStatThrowInToys_ = GenericToolbox::Json::fetchValue(_config_, "gaussStatThrowInToys", _gaussStatThrowInToys_);
@@ -242,7 +244,8 @@ std::string Propagator::getSampleBreakdownTableStr() const{
   return ss.str();
 }
 void Propagator::printBreakdowns(){
-  if( _showEventBreakdown_ ){
+
+  if( _showStagedEventBreakdown_ ){
 
     // STAGED MASK
     LogWarning << "Staged event breakdown:" << std::endl;
@@ -252,7 +255,7 @@ void Propagator::printBreakdowns(){
     ); // [iSample][iStage]
     std::vector<std::string> stageTitles;
     stageTitles.emplace_back("Sample");
-    stageTitles.emplace_back("No reweight");
+    stageTitles.emplace_back("Base weight");
     for( auto& parSet : _parManager_.getParameterSetsList() ){
       if( not parSet.isEnabled() ){ continue; }
       stageTitles.emplace_back("+ " + parSet.getName());
@@ -292,33 +295,66 @@ void Propagator::printBreakdowns(){
     }
     t.printTable();
 
+  }
+
+  if( _showEventBreakdown_ ){
     LogWarning << "Sample breakdown:" << std::endl;
     std::cout << this->getSampleBreakdownTableStr() << std::endl;
-
   }
 
   if( _showNbEventParameterBreakdown_ ){
 
-    std::map<const Parameter*, int> nbEventForParameter{}; // assuming int = 0 by default
+    struct NbEventBreakdown{
+      size_t nbTotal{0};
+      std::map<int, int> nbForSample{};
+    };
+
+    std::map<const Parameter*, NbEventBreakdown> nbEventForParameter{}; // assuming int = 0 by default
     for( auto& cache: _eventDialCache_.getCache() ){
       for( auto& dial : cache.dialResponseCacheList ){
         for( int iInput = 0 ; iInput < dial.dialInterface.getInputBufferRef()->getInputSize() ; iInput++ ){
-          nbEventForParameter[ &dial.dialInterface.getInputBufferRef()->getParameter(iInput) ] += 1;
+          nbEventForParameter[ &dial.dialInterface.getInputBufferRef()->getParameter(iInput) ].nbTotal += 1;
+
+          if( _showNbEventPerSampleParameterBreakdown_ ){
+            nbEventForParameter[ &dial.dialInterface.getInputBufferRef()->getParameter(iInput) ]
+                .nbForSample[cache.event->getIndices().sample] += 1;
+          }
         }
       }
     }
 
     GenericToolbox::TablePrinter t;
+    t << "Parameter";
+    t << GenericToolbox::TablePrinter::NextColumn << "All samples";
+
+    if( _showNbEventPerSampleParameterBreakdown_ ){
+      for( auto& sample : _sampleSet_.getSampleList() ){
+        if( not sample.isEnabled() ){ continue; }
+        t << GenericToolbox::TablePrinter::NextColumn << sample.getName();
+      }
+    }
+
+    t << GenericToolbox::TablePrinter::NextLine;
+
     for( auto& parSet : _parManager_.getParameterSetsList() ){
       if( not parSet.isEnabled() ){ continue; }
       for( auto& par : parSet.getParameterList() ){
         if( not par.isEnabled() ){ continue; }
 
         t.setColorBuffer( GenericToolbox::ColorCodes::resetColor );
-        if( nbEventForParameter[ &par ] == 0 ){ t.setColorBuffer( GenericToolbox::ColorCodes::redBackground ); }
+        if( nbEventForParameter[ &par ].nbTotal == 0 ){ t.setColorBuffer( GenericToolbox::ColorCodes::redBackground ); }
 
-        t << par.getFullTitle() << GenericToolbox::TablePrinter::NextColumn;
-        t << nbEventForParameter[ &par ] << GenericToolbox::TablePrinter::NextLine;
+        t << par.getFullTitle();
+        t << GenericToolbox::TablePrinter::NextColumn << nbEventForParameter[ &par ].nbTotal;
+
+        if( _showNbEventPerSampleParameterBreakdown_ ){
+          for( auto& sample : _sampleSet_.getSampleList() ){
+            if( not sample.isEnabled() ){ continue; }
+            t << GenericToolbox::TablePrinter::NextColumn << nbEventForParameter[ &par ].nbForSample[sample.getIndex()];
+          }
+        }
+
+        t << GenericToolbox::TablePrinter::NextLine;
       }
     }
 
