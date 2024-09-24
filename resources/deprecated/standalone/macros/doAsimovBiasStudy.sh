698d613dad9624f0bbb7a1d0ade533161553e5dd
Adrien Blanchet <adrien.blanchet@live.fr>
2024-09-24 09:38:59 +0200

    removing xsllhFitter macros and markdown files

--- resources/deprecated/standalone/macros/doAsimovBiasStudy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-source /home/dolan/.bashrc
-runnd280
-sourcefitter
-CCQEFit -i $1 -m 3 -o $2 -s ${RANDOM} -M 2 -S 1 
-# Use as:
-# # e.g for i in `seq 0 250`; do qsub doAsimovBiasStudy.sh -F "/data/t2k/dolan/software/xsLLhFitter/xsLLhFitter/inputs/NeutAir5_2DV2.root  $PWD/biasStudyAsiOut_${i}_reg0p001.root"; done;
