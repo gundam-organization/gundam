79e446fbef37432ac8e7a918daf20551ace774ad
Adrien Blanchet <adrien.blanchet@live.fr>
2024-10-19 14:25:14 +0200

    removing access to nullptr

--- src/SamplesManager/src/Histogram.cpp
+++ src/SamplesManager/src/Histogram.cpp
@@ -115,9 +115,6 @@ void Histogram::refillHistogram(int iThread_){
   LogDebug << GET_VAR_NAME_VALUE(this->isCacheManagerEnabled()) << std::endl;
   LogDebug << GET_VAR_NAME_VALUE(_cacheManagerIndex_) << std::endl;
   LogDebug << GET_VAR_NAME_VALUE(_cacheManagerValidFlagPtr_) << std::endl;
-  LogDebugIf(_cacheManagerValidFlagPtr_) << GET_VAR_NAME_VALUE(*_cacheManagerValidFlagPtr_) << std::endl;
-  // _cacheManagerValidFlagPtr_ and (*_cacheManagerValidFlagPtr_) and _cacheManagerIndex_ >= 0;
-
 //  LogThrow("debug stop");
 
   if( isCacheManagerEnabled ){
