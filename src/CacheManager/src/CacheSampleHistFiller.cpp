d61da1f2827c84a25ef9ec73a703f3b88fe9a81a
Adrien Blanchet <adrien.blanchet@live.fr>
2024-10-30 11:57:22 +0100

    fixing C++14 loop

--- src/CacheManager/src/CacheSampleHistFiller.cpp
+++ src/CacheManager/src/CacheSampleHistFiller.cpp
@@ -10,7 +10,7 @@ void CacheSampleHistFiller::pullHistContent(const double* fSumHostPtr_, const do
 #if HAS_CPP_17
   for( auto [binContent, binContext] : histPtr->loop() ){
 #else
-    for( auto element : sample.getHistogram().loop() ){ auto& binContent = std::get<0>(element); auto& binContext = std::get<1>(element);
+    for( auto element : histPtr->loop() ){ auto& binContent = std::get<0>(element); auto& binContext = std::get<1>(element);
 #endif
     binContent.sumWeights = fSumHostPtr_[cacheManagerIndexOffset + binContext.bin.getIndex()];
     binContent.sqrtSumSqWeights = sqrt(fSum2HostPtr_[cacheManagerIndexOffset + binContext.bin.getIndex()]);
