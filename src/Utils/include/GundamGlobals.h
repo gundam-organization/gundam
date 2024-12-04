96f30a351cc70250efa582ea1664b3f40083113c
Adrien Blanchet <adrien.blanchet@live.fr>
2024-12-04 16:01:36 +0100

    getting rid of global mutex and putting all global parameters into a global struct with comments

--- src/Utils/include/GundamGlobals.h
+++ src/Utils/include/GundamGlobals.h
@@ -5,32 +5,50 @@
 #ifndef GUNDAM_GUNDAM_GLOBALS_H
 #define GUNDAM_GUNDAM_GLOBALS_H
 
-#include <mutex>
-
 
 class GundamGlobals{
 
 public:
   // setters
-  static void setIsDebug(bool enable_){ _isDebug_ = enable_; }
-  static void setLightOutputMode(bool enable_){ _lightOutputModeEnabled_ = enable_; }
-  static void setIsCacheManagerEnabled(bool enable_){ _useCacheManager_ = enable_; }
-  static void setIsForceCpuCalculation(bool enable_){ _forceCpuCalculation_ = enable_; }
-  static void setNumberOfThreads(int nbCpuThreads_){ _nbCpuThreads_ = nbCpuThreads_; }
+  static void setIsDebug(bool enable_){ _parameters_.isDebug = enable_; }
+  static void setLightOutputMode(bool enable_){ _parameters_.lightOutputModeEnabled = enable_; }
+  static void setIsCacheManagerEnabled(bool enable_){ _parameters_.useCacheManager = enable_; }
+  static void setIsForceCpuCalculation(bool enable_){ _parameters_.forceCpuCalculation = enable_; }
+  static void setNumberOfThreads(int nbCpuThreads_){ _parameters_.nbCpuThreads = nbCpuThreads_; }
 
   // getters
-  static bool isDebug(){ return _isDebug_; }
-  static bool isLightOutputMode(){ return _lightOutputModeEnabled_; }
-  static bool isCacheManagerEnabled(){ return _useCacheManager_; }
-  static bool isForceCpuCalculation(){ return _forceCpuCalculation_; }
-  static int getNbCpuThreads(){ return _nbCpuThreads_; }
+  static bool isDebug(){ return _parameters_.isDebug; }
+  static bool isLightOutputMode(){ return _parameters_.lightOutputModeEnabled; }
+  static bool isCacheManagerEnabled(){ return _parameters_.useCacheManager; }
+  static bool isForceCpuCalculation(){ return _parameters_.forceCpuCalculation; }
+  static int getNbCpuThreads(){ return _parameters_.nbCpuThreads; }
 
 private:
-  static bool _isDebug_; /* enable debug printouts for the config reading */
-  static bool _useCacheManager_; /* enable the use of the cache manager in the propagator (mainly for GPU) */
-  static bool _forceCpuCalculation_; /* force using CPU in the cache manager */
-  static bool _lightOutputModeEnabled_;
-  static int _nbCpuThreads_;
+
+  struct Parameters{
+    // Debug mode is set to enable extra printouts during runtime.
+    bool isDebug{false};
+
+    // Light output mode will disable the writing of objects in the output files.
+    // This makes the produced output .root file lighter.
+    bool lightOutputModeEnabled{false};
+
+    // useCacheManager is a parameters that tells GUNDAM to use the CacheManager library
+    // to propagate parameters. This is set to true when GPU is enabled
+    // TODO: this should be a CacheManager parameter
+    bool useCacheManager{false};
+
+    // forceCpuCalculation allows to also do the propagation of parameters with the CPU
+    // routine in order to check the accuracy of the CacheManager computation
+    // TODO: this should be a CacheManager parameter
+    bool forceCpuCalculation{false};
+
+    // how many parallel threads must be running during the
+    // compatible routines during runtime
+    int nbCpuThreads{1};
+  };
+  static Parameters _parameters_;
+
 
 };
 
--- src/Utils/src/GundamGlobals.cpp
+++ src/Utils/src/GundamGlobals.cpp
@@ -4,9 +4,6 @@
 
 #include "GundamGlobals.h"
 
-// statics
-int GundamGlobals::_nbCpuThreads_{1};
-bool GundamGlobals::_useCacheManager_{false};
-bool GundamGlobals::_isDebug_{false};
-bool GundamGlobals::_forceCpuCalculation_{false};
-bool GundamGlobals::_lightOutputModeEnabled_{false};
+// static on global scope
+GundamGlobals::Parameters GundamGlobals::_parameters_{};
+
