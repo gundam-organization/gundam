4f42d3654b40e710fa3686709216757efc354534
Clark McGrew <clark.mcgrew@stonybrook.edu>
2025-06-12 09:54:30 -0400

    Construct Kriged dial using explicit entry count
    Don't depend on the size of the vector that the external library used
    to return the krigging weights.  Use the explicit number of weights
    returned by the library says it returned.

--- src/DialDictionary/DialDefinitions/src/Kriged.cpp
+++ src/DialDictionary/DialDefinitions/src/Kriged.cpp
@@ -1,12 +1,13 @@
 #include "Kriged.h"
 
 Kriged::Kriged(const std::vector<double>* table_,
-                std::vector<int> index_, std::vector<double> weight_,
-                const std::string& options_)
+               int entries_, std::vector<int> index_, std::vector<double> weight_,
+               const std::string& options_)
     : _table_{table_} {
-    _weights_.reserve(index_.size());
-    for (int i=0; i<index_.size(); ++i) {
-        _weights_.emplace_back(index_[i],weight_[i]);
+    _weights_.resize(entries_);
+    for (int i=0; i<entries_; ++i) {
+        _weights_[i].first = index_[i];
+        _weights_[i].second = weight_[i];
     }
 }
 
